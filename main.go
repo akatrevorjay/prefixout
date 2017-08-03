e54f80aae39916d19582d07ee3adb91358a8c197
Trevor Joynson (trevorj) <github@skywww.net>
2017-08-02 23:04:42 -0700

    Cleanup and colors

--- main.go
+++ main.go
@@ -1,32 +1,37 @@
 package main
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/exec"
 	"path"
+	"strings"
 	"syscall"
 
 	docopt "github.com/docopt/docopt-go"
-	"github.com/rabbit-ci/logstreamer"
+	"github.com/logrusorgru/aurora"
 )
 
 var version = "0.0.1"
 
-func main() {
-	self := path.Base(os.Args[0])
-	logger := log.New(os.Stderr, fmt.Sprintf("%s: ", self), 0)
+var self = path.Base(os.Args[0])
+var logger = log.New(os.Stderr, fmt.Sprintf("%s: ", self), 0)
 
-	orPanic := func(err error) {
-		if err == nil {
-			return
-		}
-		logger.Panic(err)
+func orPanic(err error) {
+	if err == nil {
+		return
 	}
+	logger.Panic(err)
+}
 
-	usage := `Usage: prefixout [-dt] [-p PREFIX | --prefix PREFIX] -- COMMAND [ARGS ...]
+func main() {
+	var err error
+
+	usage := `Usage: prefixout [-dtc] [-p PREFIX | --prefix PREFIX] -- COMMAND [ARGS ...]
 
 	Prefixes stdout/stderr of a command. Nuff said.
 
@@ -35,10 +40,13 @@ Arguments:
 	COMMAND		Command to exec
 	ARGS		Command arguments
 	-d			Differentiate stdout/stderr
-	-t			Timestamp output`
+	-t			Timestamp output
+	-c			Color output`
 
 	arguments, err := docopt.Parse(usage, nil, true, fmt.Sprintf("prefixout %s", version), true)
 	orPanic(err)
+
+	au := aurora.NewAurora(arguments["-c"].(bool))
 
 	exec_args := make([]string, 0)
 	if arguments["ARGS"] != nil {
@@ -55,15 +63,11 @@ Arguments:
 	exec_all = append(exec_all, exec_cmd)
 	exec_all = append(exec_all, exec_args...)
 
-	prefix := fmt.Sprintf("%s: ", exec_cmd)
+	var prefix string
 	if arguments["PREFIX"] != nil {
 		prefix = arguments["PREFIX"].(string)
-	}
-
-	out_prefix := ""
-	err_prefix := ""
-	if arguments["-d"].(bool) {
-		err_prefix = "[stderr] "
+	} else {
+		prefix = fmt.Sprintf("%s: ", exec_cmd)
 	}
 
 	prefix_flags := 0
@@ -71,24 +75,32 @@ Arguments:
 		prefix_flags |= log.Ldate | log.Ltime
 	}
 
-	logout := log.New(os.Stdout, prefix, prefix_flags)
-	logStreamerOut := logstreamer.NewLogstreamer(logout, out_prefix, false)
-	defer logStreamerOut.Close()
+	out_prefix := prefix
+	err_prefix := prefix
+	if arguments["-d"].(bool) {
+		err_prefix += "[stderr] "
+	}
 
-	logerr := log.New(os.Stderr, prefix, prefix_flags)
-	logStreamerErr := logstreamer.NewLogstreamer(logerr, err_prefix, true)
-	defer logStreamerErr.Close()
+	logout := log.New(os.Stdout, "", prefix_flags)
+	slurperOut := NewSlurper(logout, out_prefix, func(prefix string, str string) string {
+		return au.Bold(prefix).String() + str
+	})
+	defer slurperOut.Close()
+
+	logerr := log.New(os.Stderr, "", prefix_flags)
+	slurperErr := NewSlurper(logerr, err_prefix, func(prefix string, str string) string {
+		return au.Bold(prefix).Red().String() + str
+	})
+	defer slurperErr.Close()
 
 	cmd := exec.Command(exec_cmd, exec_args...)
-	cmd.Stdout = logStreamerOut
-	cmd.Stderr = logStreamerErr
-
-	// Reset any error
-	logStreamerErr.FlushRecord()
+	// Attach fds (stdin is handled by wait)
+	cmd.Stdout = slurperOut
+	cmd.Stderr = slurperErr
 
 	err = cmd.Start()
 	if err != nil {
-		log.Fatalf("ERROR! Could not spawn command. %v\n", err.Error())
+		logger.Fatalf("ERROR! Could not spawn command. %v\n", err.Error())
 	}
 
 	err = cmd.Wait()
@@ -106,7 +118,110 @@ Arguments:
 				os.Exit(code)
 			}
 		} else {
-			logger.Fatalf("cmd.Wait: %v", err)
+			orPanic(err)
 		}
 	}
+}
+
+type Slurper struct {
+	Logger    *log.Logger
+	buf       *bytes.Buffer
+	prefix    string
+	formatter func(string, string) string
+}
+
+func NewSlurper(logger *log.Logger, prefix string, formatter func(string, string) string) *Slurper {
+	l := &Slurper{
+		Logger:    logger,
+		buf:       bytes.NewBuffer([]byte("")),
+		prefix:    prefix,
+		formatter: formatter,
+	}
+
+	return l
+}
+
+func termHasColors() bool {
+	// TODO Just check if tty and/or interactive.
+
+	term := os.Getenv("TERM")
+
+	if strings.HasSuffix(term, "color") || strings.HasPrefix(term, "xterm") {
+		return true
+	}
+
+	return false
+}
+
+func (l *Slurper) Write(p []byte) (n int, err error) {
+	if n, err = l.buf.Write(p); err != nil {
+		return
+	}
+
+	err = l.OutputLines()
+	return
+}
+
+func (l *Slurper) Close() error {
+	if err := l.Flush(); err != nil {
+		return err
+	}
+	l.buf = bytes.NewBuffer([]byte(""))
+	return nil
+}
+
+func (l *Slurper) Flush() error {
+	var p []byte
+
+	count, err := l.buf.Read(p)
+	if err != nil {
+		return err
+	}
+
+	if count > 0 {
+		l.out(string(p))
+	}
+
+	return nil
+}
+
+func (l *Slurper) OutputLines() error {
+	for {
+		line, err := l.buf.ReadString('\n')
+
+		if len(line) > 0 {
+			if strings.HasSuffix(line, "\n") {
+				l.out(line)
+			} else {
+				// put back into buffer, it's not a complete line yet
+				//  Close() or Flush() have to be used to flush out
+				//  the last remaining line if it does not end with a newline
+				if _, err := l.buf.WriteString(line); err != nil {
+					return err
+				}
+			}
+		}
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (l *Slurper) out(str string) {
+	if len(str) < 1 {
+		return
+	}
+
+	if l.formatter != nil {
+		str = l.formatter(l.prefix, str)
+	}
+
+	l.Logger.Print(str)
 }
